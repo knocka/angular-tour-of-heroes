866911812e66cde21188f6b789de7c34670edb56
Paul Lin <Paul.Lin@arris.com>
2017-01-23 16:58:26 -0500

    completed 1st pass

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,18 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'my-app',
@@ -12,7 +24,8 @@ export class Hero {
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-   <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+   <li *ngFor="let hero of heroes"  [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+  
     <span class="badge">{{hero.id}}</span> {{hero.name}}
    </li>
   </ul>
@@ -75,27 +88,17 @@ export class Hero {
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]  
+`]
+
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes = HEROES;
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
