139995bdef2fb703de24af801b452437e34bb43f
Paul Lin <Paul.Lin@arris.com>
2017-01-24 10:12:55 -0500

    added hero.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -18,18 +14,21 @@ const HEROES: Hero[] = [
   { id: 20, name: 'Tornado' }
 ];
 
+
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-   <li *ngFor="let hero of heroes"  [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
-  
+   <li *ngFor="let hero of heroes"  
+   [class.selected]="hero === selectedHero" 
+   (click)="onSelect(hero)">  
     <span class="badge">{{hero.id}}</span> {{hero.name}}
    </li>
   </ul>
-
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+<!--
   <div *ngIf="selectedHero">
   <h2>{{selectedHero.name}} details!</h2>
   <div><label>id: </label>{{selectedHero.id}}</div>
@@ -38,7 +37,7 @@ const HEROES: Hero[] = [
     <input [(ngModel)]="selectedHero.name" placeholder="name"/>
   </div>
   </div>
- 
+  -->
   `,
   styles: [`
   .selected {
@@ -96,7 +95,7 @@ export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes = HEROES;
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
