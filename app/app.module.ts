27e05e1e37a34ba3f3835626d69694c07fd6c610
Paul Lin <Paul.Lin@arris.com>
2017-01-25 11:55:41 -0500

    completed the tour.

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,11 +10,13 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import { DashboardComponent  }     from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+import { HeroSearchComponent }     from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import './rxjs-extensions';
 
 @NgModule({
   imports: [
@@ -28,7 +30,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
